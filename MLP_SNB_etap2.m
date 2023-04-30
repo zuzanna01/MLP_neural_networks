e350794b616ec2a0ba742e775275e557a9594686
Zuzanna <01158303@pw.edu.pl>
2023-04-30 17:27:36 +0200

    first attempt to trian our neural network

--- MLP_SNB_etap2.m
+++ MLP_SNB_etap2.m
@@ -25,24 +25,86 @@ inputNeurons = 22;
 hiddenNeurons = 5;
 outputNeurons = 1;
 
-weightInputHidden = rand(hiddenNeurons, inputNeurons);
-weightHiddenOutput = rand(outputNeurons, hiddenNeurons);
+weightInputHidden = -0.01 + (0.02)*rand(hiddenNeurons, inputNeurons); 
+weightHiddenOutput = -0.01 + (0.02)*rand(outputNeurons, hiddenNeurons);
 
 %parametry uczenia
-w=[0 0]'; % punkt satrowy ?
-w_b=w; % baias ?
-K=1000; % ilość iteracji
+K=10000; % liczba iteracji
 eta=0.01; % wspólczynnik uczenia sieci
 alpha=0.9; % współczynni bezwładności
 
+%uczenie sieci
+for i = 1:K
+
+    n =randi([1,22]);
+    x = inputTrainData(n,:);
+
+    % oblicz pobudzenie neuronów w warstwie ukrytej
+    hiddenInput = weightInputHidden * x';
+    biasHidden = -0.1 + (0.2)*rand(hiddenNeurons, 1);
+    hiddenOutput = ReLU(hiddenInput + biasHidden);
+
+    % oblicz pobudzenie neuronów w warstwie wyjściowej
+    outputInput = weightHiddenOutput * hiddenOutput;
+    biasOutput = -0.1 + (0.2)*rand(outputNeurons, 1);
+    outputOutput = stepFunction(outputInput + biasOutput);
+
+    % obliczanie błędu
+    d = outputTrainData(n); % d - target - diagnoza lekarza
+    outputError = d - outputOutput;
+   
+    % warstwy wyjściowej
+    outputDelta = outputError;
+        
+    % warstwy ukrytej
+    hiddenDelta = (weightHiddenOutput' * outputDelta).*relu_derivative(hiddenInput) ;
+
+    % Propagacja wsteczna błędu
+    % Aktualizacja wag warstwy wyjściowej
+    weightHiddenOutput = weightHiddenOutput + eta * outputDelta * hiddenOutput';
+        
+    % Aktualizacja wag warstwy ukrytej
+    weightInputHidden = weightInputHidden + (eta * hiddenDelta) * x;
+
+end
+
+right_diagnosis =0;
+%walidacja
+for i = 1:size(inputTestData,1)
+    
+    % Pobieranie i-tego wiersza danych testowych
+    x = inputTestData(i,:);
+    
+    % Obliczanie pobudzenia neuronów warstwy ukrytej i wyjściowej
+    hiddenLayerInput = x * weightInputHidden';
+    hiddenLayerOutput = ReLU(hiddenLayerInput);
+    outputLayerInput = hiddenLayerOutput * weightHiddenOutput';
+    outputLayerOutput = stepFunction(outputLayerInput);
+    
+    % Wyświetlanie prognozy wyjściowej i rzeczywistej wartości wyjściowej
+    disp(['Prognozowane wyjście: ' num2str(outputLayerOutput) ', Rzeczywiste wyjście: ' num2str(outputTestData(i))]);
+
+    if (outputLayerOutput == outputTestData(i)) 
+        right_diagnosis = right_diagnosis+1;
+    end
+end
+
+disp(['Poprawne diagnizy: ',num2str(right_diagnosis/size(inputTestData,1)*100),' %']);
+
 %funkcja aktywacji 
 % skok jednostkowy
-function y = unit_step(x)
+function y = stepFunction(x)
 y = zeros(size(x));
-y(x >= 0) = 1;
+y(x > 0) = 1;
 end
 
 %RELU
-function y = relu(x)
+function y = ReLU(x)
 y = max(0, x);
+end
+
+% pochodna ReLU 
+function d = relu_derivative(x)
+d = x >= 0;  % wartości większe od zera mają pochodną równą 1
+d(x <= 0) = 0;  % wartości mniejsze lub równe zeru mają pochodną równą 0
 end
